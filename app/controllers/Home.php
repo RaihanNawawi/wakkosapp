2aa16c34b1e05646e448cb1da7f18c0eb7f4aaf1
Raihan Nawawi <143898535+RaihanNawawi@users.noreply.github.com>
2026-07-07 11:03:13 +0700

    Feat: (#1 START MVP View Controller) [Membuat method view di class Controller sebagai main controller view dan contoh implementasi penggunaan method didalam routing halaman]

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -1,12 +1,8 @@
 <?php
 
-class Home {
+class Home extends Controller {
     // method index akan dijalankan secara default ketika controller home dipanggil
     public function index() {
-        echo "Home/index";
-    }
-    // method test akan dijalankan ketika controller home dipanggil dengan method test, contoh: ?url=home/test
-    public function test() {
-        echo "Home/test";
+        $this->view('home/index');
     }
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -1,6 +1,8 @@
 <?php
 class Controller {
-    public function __construct() {
-        echo "Controller class has been initialized. <hr>";
+     // method view digunakan untuk memanggil file view yang berada di folder app/views
+    public function view($view, $data = []) {
+        // memanggil file view yang berada di folder app/views
+        require_once '../app/views/' . $view . '.php';
     }
 }
